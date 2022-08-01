3e471c4523a04b031e123640a65999de3273c3c2
BurhanuddinRaja <mails4burhan@gmail.com>
2022-08-01 09:27:15 +0530

    final commit

--- routes/files.js
+++ routes/files.js
@@ -66,7 +66,7 @@ router.post('/send', async (req, res) => {
     sendEmail({
         from: emailFrom,
         to: emailTo,
-        subject: 'ImageShare',
+        subject: 'fileShare',
         text: `${emailFrom} has shared an email with you.`,
         html: require('../services/emailTemplate')( {
             text: `${emailFrom} has shared an email with you.`,
